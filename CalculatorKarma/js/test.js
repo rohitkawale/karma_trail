f7c209bff85d6cf8b847f0d09455880531a1127a
rohit kawale <rohit.kawale02@gmail.com>
2016-06-24 16:00:08 +0530

    karma added

---
+++ CalculatorKarma/js/test.js
@@ -0,0 +1,45 @@
+var app= angular.module('myApp',['daterangepicker']);
+
+app.controller('myCtrl',function($scope){
+$scope.data={};
+$scope.users=[{
+	'name' : "rohit",
+	'surname' : "kawale"
+},
+{
+	'name' : "ankitesh",
+	'surname' : "gupta"
+}
+];
+$scope.datePicker={};
+$scope.datePicker.date = {
+	startDate: moment().subtract(5, "days"),
+    endDate: moment()
+};
+
+$scope.edit= function(u){
+	$scope.data=u;
+
+};
+$scope.save= function(){
+	$scope.data={};
+};
+$scope.delete=function(index){
+	$scope.users.splice(index,1);
+
+}
+
+$scope.adduser= function(){
+	var datePicker=$scope.datePicker.date ;
+	console.log(datePicker);
+
+	var user={
+		'name' : $scope.data.name,
+		'surname' : $scope.data.surname
+	};
+	$scope.users.push(user);
+	$scope.data={};
+	console.log($scope.users);
+}
+
+});
